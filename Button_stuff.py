9b79e6d931783c639a73b925003124d448a76f8a
zhouxi <zhouxi@wgc.school.nz>
2019-02-15 10:11:13 +1300

    Initial commit

---
+++ Button_stuff.py
@@ -0,0 +1,16 @@
+import tkinter
+root = tkinter.Tk()
+
+def print_hi():
+    print("hi")
+
+
+button1 = tkinter.Button(root)
+button1.config(text = "Morning tea soon.",
+               bg = "green",
+               fg = "hotpink",
+               font = ("Papyrus", "50"),
+               command = print_hi)
+button1.grid()
+
+root.mainloop()
--- Helloworld.py
+++ Helloworld.py
@@ -2,21 +2,21 @@ import tkinter
 root = tkinter.Tk()
 
 root.title("Hello World!!")
-root.geometry("600x200")
+root.geometry("600x500")
 
 # create a label
 my_label = tkinter.Label(root)
-my_label.config(text = "Hello!", fg = "green")
+my_label.config(text = "Hello!", fg = "green", bg = "yellow", font = ("Wid Latin","50","italic"))
 my_label.grid()
 
 # create a label
 my_label2 = tkinter.Label(root)
-my_label2.config(text = "Hey there!", fg = "blue")
+my_label2.config(text = "Hey there!", fg = "blue", bg = "white", font = ("Stencil", "50"), padx = "100")
 my_label2.grid()
 
 # create a label
 my_label3 = tkinter.Label(root)
-my_label3.config(text = "Hey you!", fg = "pink")
+my_label3.config(text = "Hey you!", fg = "pink", bg = "blue", font = ("Times", "50", "bold"), pady = "30")
 my_label3.grid()
 
 root.mainloop()
