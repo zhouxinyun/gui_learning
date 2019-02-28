be862d68650235b594382fcd6f339ec2ec691318
zhouxi <zhouxi@wgc.school.nz>
2019-02-28 15:16:32 +1300

    initial commit

---
+++ image_stuff.py
@@ -0,0 +1,22 @@
+from __future__ import print_function
+
+import sys
+
+sys.path.append("N:\python-modules")
+
+
+from PIL import Image
+from PIL import ImageFilter
+from PIL import ImageEnhance
+
+imagepath = input("What is the name of the image file?")
+
+try:
+    im = Image.open(imagepath)
+
+
+except IOError:
+    print("Cannot read file.")
+value = float(input("I can adjust contrast. Give me a value from 0 to 1(e.g. 0.5)"))
+im = ImageEnhance.Contrast(im).enhance(value)
+im.save("enhanced.jpg")
