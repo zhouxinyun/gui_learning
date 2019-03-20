26dbc8bb57817b6441d9f3c5134572498a0b93fe
zhouxi <zhouxi@wgc.school.nz>
2019-03-20 14:35:15 +1300

    Add shopping list

--- Student_Management_System.py
+++ Student_Management_System.py
@@ -66,29 +66,12 @@ class MyStudent:
 from tkinter import *
 
 student_list = []
-student_list.append(MyStudent("Jacqui", 68, "0800", "purple"))
+student_list.append(MyStudent("Jacqui", 68, "0800dominos", "purple"))
 student_list.append(MyStudent("Zara", 34, "1234", "red"))
-student_list.append(MyStudent("Eliza", 17, "47325", "orange"))
-student_list.append(MyStudent("Hanna", 18, "55325335", "black"))
-student_list.append(MyStudent("Lina", 5, "4512565", "black"))
-student_list.append(MyStudent("Cate", 12, "45645453", "yellow"))
-root = Tk()
-
-root.title("Manage Your Students")
-root.geometry("800x600")
-root.option_add("*Font", "LucidaGrande 20")
-root.config(padx=10, pady=10)
-
-status_text = StringVar("")
-status_text.set("a message")
-
-student_selector = Listbox(root, height=10)
-student_selector.grid(row=0, column=0, rowspan=10)
-
-
-def update_list_students():
-    for student in student_list:
-        student_selector.insert(END, student.get_name())
+student_list.append(MyStudent("Eliza", 18, "47325", "orange"))
+student_list.append(MyStudent("Hanna", 17, "384278346891", "pink"))
+student_list.append(MyStudent("Lina", 5, "5678", "black"))
+student_list.append(MyStudent("Cate", 12, "999999999", "yellow"))
 
 
 def get_student(fname):
@@ -98,54 +81,35 @@ def get_student(fname):
     print("Student not found")
 
 
-def view_details():
-    view_window = Toplevel(root)
-    view_window.title("View student details")
-
-    student_name = update_list_students().get(ACTIVE)
+def update_details():
+    student_name = student_selector.get(ACTIVE)
     current_student = get_student(student_name)
+    str_name.set("Name: " + current_student.get_name())
+    str_age.set("Name: " + current_student.get_age())
+    str_colour.set("Name: " + current_student.get_colour())
+    str_phone.set("Name: " + current_student.get_phone())
 
-    str_name = StringVar("")
-    str_name.set(current_student.get_name())
-    str_age = StringVar("")
-    str_age.set(current_student.get_age())
-    str_phone = StringVar("")
-    str_phone.set(current_student.get_phone())
-    str_colour = StringVar("")
-    str_colour.set(current_student.get_colour())
-
-    full_name = Label(view_window, text="Name: ")
-    full_name.grid(row=0, column=1, sticky=W)
-    full_name_val = Label(view_window, textvariable=str_name)
-    full_name_val.grid(row=0, column=1, sticky=W)
-
-    age = Label(view_window, text="Age: ")
-    age.grid(row=1, column=1, sticky=W)
-    age_val = Label(root, textvariable=str_age)
-    age_val.grid(row=1, column=1, sticky=W)
-
-    phone_number = Label(view_window, text="Phone Number: ")
-    phone_number.grid(row=2, column=1, sticky=W)
-    phone_number_val = Label(root, textvariable=str_phone)
-    phone_number_val.grid(row=2, column=1, sticky=W)
-
-    colour = Label(view_window, text="Colour: ")
-    colour.grid(row=3, column=1, sticky=W)
-    colour_val = Label(root, textvariable=str_colour)
-    colour_val.grid(row=3, column=1, sticky=W)
+def edit_student():
+    edit_window = Toplevel(root)
 
 
-def new_student():
-    new_window = Toplevel(root)
-    new_window.title("New student")
+root = Tk()
+
+root.title("Manage Your Students")
+root.geometry("800x600")
+root.option_add("*Font", "LucidaGrande 20")
+
+student_selector = Listbox(root, height=10)
+for student in student_list:
+    student_selector.insert(END, student.get_name())
+student_selector.grid(row=0, column=0, rowspan=10)
+
+update_button = Button(root, text="Update details>>", command=lambda: update_details())
+update_button.grid(row=10, column=0, sticky=E + W)
+
+edit_details = Button(root, text="Edit student details", command+lambda:edit_student())
+edit_details.grid(row=4, column=1, sticky =E+W)
 
 
-def remove_student():
-    pass
-
-
-btn_view_details = Button(root, text="View student details", command=lambda: view_details(), width=20)
-btn_view_details.grid(row=4, column=1, sticky=E + W, padx=10)
-update_button = Button(root, text="Update details >>").grid(row=10, column=0, sticky=E + W)
 
 root.mainloop()
